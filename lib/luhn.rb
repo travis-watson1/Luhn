0ec5c2ba0be675171069d009c6889b1f17cc3a85
Travis Watson <watsonjamest@gmail.com>
2019-08-07 14:52:53 -0500

    Final commit

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,36 +1,39 @@
 module Luhn
     def self.is_valid?(number)
-        # Break the credit card into single digits
+
         single_digit_array = number.to_s.each_char.map{ |x| x.to_i }
 
-        
-        #Start from right, double every second digit)
-        single_digit_array.each_with_index do |num, index|
-            if single_digit_array.length.even?
-                if index.even?
-                    single_digit_array[index] = num * 2
-                end
-            end
-            if single_digit_array.length.odd?
-                if index.odd?
-                    single_digit_array[index] = num * 2
-                end
-            end
-        end
+        double_every_second_digit(single_digit_array)
 
-        # If doubled value is greater than or equal to 10, subtract 9 from the value
-        single_digit_array.each_with_index do |x, idx|
-            if x >= 10
-                single_digit_array[idx] = x - 9
-            end
-        end
-              
-        # If sum is divisible by 10, its valid...otherwise it's not.
+        greater_than_ten_subtract(single_digit_array)
+        
         single_digit_array.sum % 10 === 0 ? true : false
     end
 
-    
+    def self.double_every_second_digit(arr)
+        arr.each_with_index do |num, index|
+            if arr.length.even?
+                if index.even? || index == 0
+                    arr[index] = num * 2
+                end
+            end
+            if arr.length.odd?
+                if index.odd?
+                    arr[index] = num * 2
+                end
+            end
+        end
+    end
+
+    def self.greater_than_ten_subtract(arr)
+        arr.each_with_index do |x, idx|
+            if x >= 10
+                arr[idx] = x - 9
+            end
+        end
+    end
 end
+
 
 
 
